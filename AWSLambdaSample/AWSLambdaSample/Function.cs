7b2a49628d9abb03c9c066ffc3408cf52132312c
Tan <tanchangliang@gmail.com>
2017-08-06 17:16:10 +0900

    OK For Create File On S3

--- AWSLambdaSample/AWSLambdaSample/Function.cs
+++ AWSLambdaSample/AWSLambdaSample/Function.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 using Amazon.S3;
 using Amazon.S3.Model;
-
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -21,21 +18,22 @@ namespace AWSLambdaSample
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public async Task<string> FunctionHandler(string input, ILambdaContext context)
         {
-            //IAmazonS3 client = new AmazonS3Client(Amazon.RegionEndpoint.APNortheast1);
-            AmazonS3Client client = new AmazonS3Client();
-            PutObjectRequest request = new PutObjectRequest();
-            
-            request.BucketName = "databucket-rest-datasource";
-            request.Key = "hello.txt";
-            request.ContentType = "text/plain";
-            request.ContentBody = "Hello World!";
-            try {
-                client.PutObjectAsync(request);
-                
-                return client.PutObjectAsync(request).ToString();
+            AmazonS3Client client = new AmazonS3Client(Amazon.RegionEndpoint.APNortheast1);
 
+            PutObjectRequest request = new PutObjectRequest()
+            {
+
+                BucketName = "databucket-rest-datasource",
+                Key = "hello.txt",
+                ContentType = "text/plain",
+                ContentBody = "Hello World!"
+            };
+            try {
+                await client.PutObjectAsync(request);
+                return "OK";
+                
             } catch (Exception e) {
                 return e.ToString();
             }
